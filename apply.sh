44b3f9988be7d49d03bf03256a432d9716c862f6
Shin Hirasawa <shindex@ShinnoMacBook-puro.local>
2019-07-09 16:23:42 +0900

    apply

---
+++ apply.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl apply -f nginx/nginx-deployment.yaml
+kubectl apply -f bluehub-gateway/bluehub-gateway-deployment.yaml
+kubectl apply -f bluehub-router/bluehub-router-deployment.yaml
+kubectl apply -f private-registry/private-registry-deployment.yaml
+kubectl apply -f nginx/nginx-service.yaml
+kubectl apply -f bluehub-gateway/bluehub-gateway-service.yaml
+kubectl apply -f bluehub-router/bluehub-router-service.yaml
+kubectl apply -f private-registry/private-registry-service.yaml
